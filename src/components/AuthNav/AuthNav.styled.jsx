b8db910ffc7da9ab9c7212c8243b8285e98fd092
Hakkar <zugrutus@gmail.com>
2023-05-10 14:05:01 +0100

    add-style

--- src/components/AuthNav/AuthNav.styled.jsx
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -2,4 +2,5 @@ import styled from "@emotion/styled";
 
 export const ButtonCss = styled.button`
   background-color: ${({ theme }) => theme.colors.yellow};
+  font-size: ${({theme}) => theme.spacing(9)}px;
 `;
