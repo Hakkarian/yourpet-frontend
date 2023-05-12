fd2833b3a39499921661eb9663a929b0cda1229a
Hakkar <zugrutus@gmail.com>
2023-05-12 20:59:58 +0100

    register-part

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,21 +1,21 @@
-import {useState} from "react";
+// import {useState} from "react";
 
-import {ReactComponent as OutIcon} from '../../icons/logout.svg';
-import {OutBtn, OutWrap, OutText} from './Logout.styled';
+// import {ReactComponent as OutIcon} from '../../icons/logout.svg';
+// import {OutBtn, OutWrap, OutText} from './Logout.styled';
 
-const Logout = () => {
-    const [isOpenModal, setIsOpenModal] = useState(false);
+// const Logout = () => {
+//     // const [isOpenModal, setIsOpenModal] = useState(false);
 
 
 
-    return (
-        <OutWrap>
-            <OutBtn type="button" onClick={() => setIsOpenModal(true)}>
-                <OutIcon />
-            </OutBtn>
-            <OutText>Log Out</OutText>
-        </OutWrap>
-    )
-};
+//     return (
+//         <OutWrap>
+//             <OutBtn type="button" onClick={() => setIsOpenModal(true)}>
+//                 <OutIcon />
+//             </OutBtn>
+//             <OutText>Log Out</OutText>
+//         </OutWrap>
+//     )
+// };
 
-export default Logout;
+// export default Logout;
