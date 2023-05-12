2173296bd174cff077615dff24df4f4ff600cfc5
Na-diia <108739131+Na-diia@users.noreply.github.com>
2023-05-12 15:06:16 +0300

    add userDataItem  and Logout

---
+++ src/components/Logout/Logout.styled.jsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const OutBtn = styled.button`
+ cursor: pointer;
+ border: none;
+ background-color: transparent;
+`;
+
+export const OutWrap = styled.div`
+ margin-top: 30px;
+ display: flex;
+`;
+
+export const OutText = styled.p`
+font-weight: 500;
+font-size: 16px;
+line-height: 1.37;
+letter-spacing: 0.04em;
+color: #888888;
+`;
