26eed2174bb3057e22b0bf18055c6fd928447401
Serhii Markov <goo.komalel@gmail.com>
2023-05-12 09:48:24 +0300

    created tab and desc main!

--- src/pages/MainPage/MainPage.styled.jsx
+++ src/pages/MainPage/MainPage.styled.jsx
@@ -1,9 +1,17 @@
 import styled from "@emotion/styled";
 
-import bg_mobile_pets from "./bg_img/bg_mobile_pets_full_original.png";
-import bg_mobile_paws from "./bg_img/bg_mobile_paws_full_original.png";
-import bg_mobile_pets_2x from "./bg_img/bg_mobile_pets_full_2x_original.png";
-import bg_mobile_paws_2x from "./bg_img/bg_mobile_paws_full_2x_original.png";
+import bg_mobile_pets from "./bg_img/mobile/bg_mobile_pets_full_original.png";
+import bg_mobile_paws from "./bg_img/mobile/bg_mobile_paws_full_original.png";
+import bg_mobile_pets_2x from "./bg_img/mobile/bg_mobile_pets_full_2x_original.png";
+import bg_mobile_paws_2x from "./bg_img/mobile/bg_mobile_paws_full_2x_original.png";
+import bg_tab_pets from "./bg_img/tab/bg_tablet_pets_full_original.png";
+import bg_tab_paws from "./bg_img/tab/bg_tablet_paws_full_original.png";
+import bg_tab_pets_2x from "./bg_img/tab/bg_tablet_pets_full_2x_original.png";
+import bg_tab_paws_2x from "./bg_img/tab/bg_tablet_paws_full_2x_original.png";
+import bg_desc_pets from "./bg_img/desc/bg_desc_pets_full_original.png";
+import bg_desc_paws from "./bg_img/desc/bg_desc_paws_full_original.png";
+import bg_desc_pets_2x from "./bg_img/desc/bg_desc_pets_full_2x_original.png";
+import bg_desc_paws_2x from "./bg_img/desc/bg_desc_paws_full_2x_original.png";
 
 export const SectionTag = styled.section`
     position: absolute;
@@ -55,11 +63,79 @@ export const SectionTag = styled.section`
 
 
     @media (min-width: 767px) {
+        min-height: calc(100vh - 148px);
         padding: 148px 32px 0px 32px;
+
+        
+        background-image:url(${bg_tab_paws});
+        @media (min-device-pixel-ratio: 2),
+            (min-resolution: 192dpi),
+            (min-resolution: 2dppx) {
+            background-image:
+                url(${bg_tab_paws_2x});
+            }
 
         h1 {
             font-size: 68px;
             line-height: calc(100 / 68);
+        }
+        div {
+            top: 375px;
+            min-height: 684px;
+
+            // background-color: red;
+            background-image:url(${bg_tab_pets});
+            background-position: 46% -112px;
+            background-size: 986px 982px;
+            @media (min-device-pixel-ratio: 2),
+                (min-resolution: 192dpi),
+                (min-resolution: 2dppx) {
+                background-image:
+                    url(${bg_tab_pets_2x});
+                }
+        }
+    }
+
+
+    @media (min-width: 1279px) {
+        // min-height: calc(100vh - 148px);
+        max-width:1280px;
+        padding: 100px 0px 0px 16px;
+        margin-right: auto;
+        margin-left: auto;
+
+        
+        background-image:url(${bg_desc_paws});
+        @media (min-device-pixel-ratio: 2),
+            (min-resolution: 192dpi),
+            (min-resolution: 2dppx) {
+            background-image:
+                url(${bg_desc_paws_2x});
+            }
+
+        h1 {
+            width: 505px;
+            margin-top: 188px;
+
+            // font-weight: 800;
+            font-size: 68px;
+            line-height: 130%;
+        }
+        div {
+            top: 110px;
+            left:363px;
+            min-height: 684px;
+
+            // background-color: red;
+            background-image:url(${bg_desc_pets});
+            background-position: 50% -104px;
+            background-size: 954px 975px;
+            @media (min-device-pixel-ratio: 2),
+                (min-resolution: 192dpi),
+                (min-resolution: 2dppx) {
+                background-image:
+                    url(${bg_desc_pets_2x});
+                }
         }
     }
 `;
