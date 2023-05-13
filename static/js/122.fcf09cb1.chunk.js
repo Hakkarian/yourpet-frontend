b6e68f2f47df11de9fc570b2ffaaa63c436bd05b
Hakkarian <51460023+Hakkarian@users.noreply.github.com>
2023-05-13 21:08:52 +0000

    Deploying to gh-pages from @ Hakkarian/yourpet-frontend@c82ce2830b628f7500aab15501c1f0e83ef7ac3e 🚀

--- static/js/122.fcf09cb1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[122],{2122:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});r(2791);var n=r(184),u=function(){return(0,n.jsx)("div",{children:"AddPetPage"})}}}]);
-//# sourceMappingURL=122.fcf09cb1.chunk.js.map
