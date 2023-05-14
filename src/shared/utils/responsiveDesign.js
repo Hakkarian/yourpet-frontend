7e9c0bba017736304226cc029f830f63242aef76
Hakkar <zugrutus@gmail.com>
2023-05-14 19:30:30 +0100

    useful-utils

--- src/shared/utils/responsiveDesign.js
+++ src/shared/utils/responsiveDesign.js
@@ -1,5 +1,5 @@
 const { css } = require('@emotion/react');
-const { default: styled } = require('@emotion/styled');
+// const { default: styled } = require('@emotion/styled');
 
 const responsiveMixin = (property, values) => {
   const breakpoints = {
@@ -22,21 +22,23 @@ const responsiveMixin = (property, values) => {
   `;
 };
 
-const Title = styled.h1`
-  font-size: 24px;
+// ----------- EXAMPLE OF USAGE ---------------
 
-  ${responsiveMixin('font-size', { sm: '28px', md: '32px', xl: '36px' })}
-`;
-const Paragraph = styled.p`
-  font-size: 16px;
-  ${responsiveMixin('font-size', { sm: '12px', md: '16px', xl: '18px' })}
-`;
+// const Title = styled.h1`
+//   font-size: 24px;
 
-const Div = styled.div`
-  width: 44px;
-  background-color: red;
-  ${responsiveMixin('width', { sm: '36px', md: '44px', xl: '60px' })}
-  ${responsiveMixin('background-color', { sm: 'green', md: 'red', xl: 'yellow' })}
-`;
+//   ${responsiveMixin('font-size', { sm: '28px', md: '32px', xl: '36px' })}
+// `;
+// const Paragraph = styled.p`
+//   font-size: 16px;
+//   ${responsiveMixin('font-size', { sm: '12px', md: '16px', xl: '18px' })}
+// `;
+
+// const Div = styled.div`
+//   width: 44px;
+//   background-color: red;
+//   ${responsiveMixin('width', { sm: '36px', md: '44px', xl: '60px' })}
+//   ${responsiveMixin('background-color', { sm: 'green', md: 'red', xl: 'yellow' })}
+// `;
 
 export default responsiveMixin;
