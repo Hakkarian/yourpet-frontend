65b62adffc2d4c4faea9f727ca2c953236b9be32
Olena Bondar <olena.bondar1060@gmail.com>
2023-05-14 20:24:53 +0300

    add components to NewsPage

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
+import newsItems from '../../components/NewsList/news.json';
+
+import { NewsList } from 'components/NewsList/NewsList';
+import { Title } from './NewsPage.styled';
 const NewsPage = () => {
   return (
-    <div>NewsPage</div>
-  )
-}
+    <>
+      <Title>News</Title>
+      <NewsList news={newsItems} />
+    </>
+  );
+};
 
-export default NewsPage
+export default NewsPage;
---
+++ src/pages/NewsPage/NewsPage.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 40px;
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 33px;
+  letter-spacing: -0.01em;
+  @media screen and (min-width: 480px) {
+    font-size: 24px;
+  }
+`;
