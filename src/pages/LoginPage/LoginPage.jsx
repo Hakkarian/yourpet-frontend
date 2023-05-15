437f0cc49bc4d2d51a182db82689ae2bbfc69dd3
Hakkar <zugrutus@gmail.com>
2023-05-15 20:44:51 +0100

    fix

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CenrteredDiv, FormContainer } from './LoginPage.styled'
-import LoginForm from 'components/LoginForm';
+// import LoginForm from 'components/LoginForm';
 
 const LoginPage = () => {
   return (
