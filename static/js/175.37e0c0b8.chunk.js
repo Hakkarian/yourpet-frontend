3867c197746120ae29c19cfd5905b26643096b32
Hakkarian <51460023+Hakkarian@users.noreply.github.com>
2023-05-15 12:11:34 +0000

    Deploying to gh-pages from @ Hakkarian/yourpet-frontend@fbe69b9a25548e7cbdf1b75caf0f7f24ee3115a8 🚀

--- static/js/175.37e0c0b8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[175],{7175:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});n(2791);var r=n(184),u=function(){return(0,r.jsx)("div",{children:"LoginPage"})}}}]);
-//# sourceMappingURL=175.37e0c0b8.chunk.js.map
