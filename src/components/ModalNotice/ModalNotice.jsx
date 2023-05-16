7784d295d588344409f8ba642174a32ee1d79a15
IrynaKniazieva <gleb0812@gmail.com>
2023-05-17 02:15:21 +0300

    fixed

--- src/components/ModalNotice/ModalNotice.jsx
+++ src/components/ModalNotice/ModalNotice.jsx
@@ -1,81 +1,3 @@
-
-
-// import {
-//   ContactsLink,
-//   Container,
-//   Img,
-//   Title,
-//   List,
-//   Item,
-//   Text,
-//   BtnContainer,
-// } from './ModalNotice.styled';
-// // import CrossButton from 'shared/components/CrossButton/CrossButton';
-
-// import { ReactComponent as HeartIcon } from '../../icons/heart.svg';
-
-// import Button from 'shared/components/Button/Button';
-// import defaultImage from '../../images/default-user-img.jpg';
-
-
-
-// const ModalNotice = () => {
-  
-//   return (
-//         <div>
-//           <Container>
-//           <Img
-//       src={defaultImage}
-//       alt='User'/>
-//           <div>
-//             <Title>Title</Title>
-//             <List>
-//               <Item>
-//                 <Text>Name:</Text>
-//               </Item>
-//               <Item>
-//                 <Text>Birthday:</Text>
-//               </Item>
-//               <Item>
-//                 <Text>Breed:</Text>
-//               </Item>
-//               <Item>
-//                 <Text>Place:</Text>
-//               </Item>
-//               <Item>
-//                 <Text>The sex:</Text>
-//               </Item>
-//               <Item>
-//                 <Text>Email:</Text>
-//               </Item>
-//               <Item>
-//                 <Text>Phone:</Text>
-//               </Item>
-//             </List>
-//           </div>
-//           </Container>
-
-//           <Text>Coments:</Text>
-
-//           <BtnContainer>
-        
-                
-//           <Button type="button" color="blue" width="129px">
-//             Add to <HeartIcon width="24px" height="24px" />
-//           </Button>
-
-//           <ContactsLink>Contacts</ContactsLink>
-//           </BtnContainer>
-//         </div>
-  
-//   );
-// };
-
-// export default ModalNotice;
-
-
-
-
 import React from 'react';
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
@@ -83,8 +5,6 @@ import { createPortal } from 'react-dom';
 import {
   Backdrop,
   Wrapper,
-  CloseModalBtn,
-  ContactsLink,
   Container,
   Img,
   Title,
@@ -94,8 +14,10 @@ import {
   BtnContainer,
 } from './ModalNotice.styled';
 // import CrossButton from 'shared/components/CrossButton/CrossButton';
-import { ReactComponent as CrossIcon } from '../../icons/cross-small.svg';
+// import { ReactComponent as CrossIcon } from '../../icons/cross-small.svg';
 import { ReactComponent as HeartIcon } from '../../icons/heart.svg';
+
+import CrossButton from 'shared/components/CrossButton/CrossButton';
 
 import Button from 'shared/components/Button/Button';
 import defaultImage from '../../images/default-user-img.jpg';
@@ -122,56 +44,50 @@ const ModalNotice = ({ onClose, children }) => {
   return createPortal(
     <Backdrop onClick={handleBackdropClick}>
       <Wrapper>
-        <CloseModalBtn type="button" onClick={onClose}>
-          <CrossIcon />
-        </CloseModalBtn>
-        {/* <CrossButton type='button' onClick={onClose}/> */}
+        <CrossButton type="button" onClick={onClose} />
         <div>
           <Container>
-          <Img
-      src={defaultImage}
-      alt='User'/>
-          <div>
-            <Title>Title</Title>
-            <List>
-              <Item>
-                <Text>Name:</Text>
-              </Item>
-              <Item>
-                <Text>Birthday:</Text>
-              </Item>
-              <Item>
-                <Text>Breed:</Text>
-              </Item>
-              <Item>
-                <Text>Place:</Text>
-              </Item>
-              <Item>
-                <Text>The sex:</Text>
-              </Item>
-              <Item>
-                <Text>Email:</Text>
-              </Item>
-              <Item>
-                <Text>Phone:</Text>
-              </Item>
-            </List>
-          </div>
+            <Img src={defaultImage} alt="User" />
+            <div>
+              <Title>Title</Title>
+              <List>
+                <Item>
+                  <Text>Name:</Text>
+                </Item>
+                <Item>
+                  <Text>Birthday:</Text>
+                </Item>
+                <Item>
+                  <Text>Breed:</Text>
+                </Item>
+                <Item>
+                  <Text>Place:</Text>
+                </Item>
+                <Item>
+                  <Text>The sex:</Text>
+                </Item>
+                <Item>
+                  <Text>Email:</Text>
+                </Item>
+                <Item>
+                  <Text>Phone:</Text>
+                </Item>
+              </List>
+            </div>
           </Container>
 
           <Text>Coments:</Text>
 
           <BtnContainer>
-        
-                
-          <Button type="button" color="blue" width="129px">
-            Add to <HeartIcon width="24px" height="24px" />
-          </Button>
+            <Button className="btn" type="button" color="blue" width="129px">
+              Add to <HeartIcon width="24px" height="24px" />
+            </Button>
+            <Button className="btn" type="button" color="white" width="129px">
+              <a href="tel:+380961111111">Contacts</a>
+            </Button>
 
-          <ContactsLink>Contacts</ContactsLink>
+            {/* <ContactsLink>Contacts</ContactsLink> */}
           </BtnContainer>
-
-          
         </div>
       </Wrapper>
     </Backdrop>,
