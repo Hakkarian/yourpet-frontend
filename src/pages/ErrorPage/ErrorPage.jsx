fb0fc4047fc5594d598c4ad8650ce7607dd96c04
Serhii Markov <goo.komalel@gmail.com>
2023-05-16 17:17:07 +0300

    some correct!
    - Main
    -404
    -added modal-wrapper

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 
+import Button from 'shared/components/Button/Button';
 import { DivTag } from './errorPage.styled';
+import { ReactComponent as Paw } from '../../icons/paw.svg';
 
 const ErrorPage = () => {
   return (
     <DivTag>
       <h2>{`Ooops! This page not found :(`}</h2>
-      <div></div>
+      <div className='cat_div'>
+        <MediaQuery minWidth={1280}>
+          <div className='yellow_round_div'></div>
+          {/* <div className='yellow_round_back_div'></div> */}
+        </MediaQuery>
+      </div>
       <Link to="/">
-        <button type="button">To main page</button>
+        <Button type="button" color="blue" width="248px">
+          {"To main page"}
+          <Paw></Paw>
+        </Button>
       </Link>
     </DivTag>
   )
