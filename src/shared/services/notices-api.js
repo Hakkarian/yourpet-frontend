83d9666c6792e1e6dacdba492a775f927984c65c
Hakkar <zugrutus@gmail.com>
2023-05-17 17:44:27 +0100

    token-comment

--- src/shared/services/notices-api.js
+++ src/shared/services/notices-api.js
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 // });
 
 import { instance } from './auth-api';
-instance.defaults.headers.authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NjNkNzdlZDI0MWU1M2YyNWM0Y2ZjYiIsImlhdCI6MTY4NDI2NDg0MCwiZXhwIjoxNjg2ODU2ODQwfQ.QriSZSrYZ-Fj-QJKL2GJqCPmN4i2BG-x1bBNwiSBy9Y`;
+// instance.defaults.headers.authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NjNkNzdlZDI0MWU1M2YyNWM0Y2ZjYiIsImlhdCI6MTY4NDI2NDg0MCwiZXhwIjoxNjg2ODU2ODQwfQ.QriSZSrYZ-Fj-QJKL2GJqCPmN4i2BG-x1bBNwiSBy9Y`;
 
 export const searchPetByQuery = async query => {
   try {
