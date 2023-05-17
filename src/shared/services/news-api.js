7879d28c1e2910215900c3cb90417764bff1df0c
Olena Bondar <olena.bondar1060@gmail.com>
2023-05-17 13:03:24 +0300

    fix search query

--- src/shared/services/news-api.js
+++ src/shared/services/news-api.js
@@ -5,27 +5,13 @@ export const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'https://barkend.onrender.com/api',
 });
 
-export const getNews = async (page = 1, limit = 6, query = '') => {
+export const getNews = async params => {
   try {
-    const response = await instance.get(
-      `/news/title?page=${page}&limit=${limit}&search=${query}`
-    );
+    const response = await instance.get('/news/title', { params });
+
     console.log(response.data);
     return response.data;
   } catch (error) {
     toast.error(error.message);
   }
 };
-
-// export const searchNewsByQuery = async (page = 1, limit = 6, query = '') => {
-//   try {
-//     const response = await instance.get(
-//       `/news/title?page=${page}&limit=${limit}&search=${query}`
-//     );
-//     console.log(response.data);
-
-//     return response.data;
-//   } catch (error) {
-//     toast.error(error.message);
-//   }
-// };
