d48eede73b9a1dc9c2ac1b83b35e3e6381b5df9d
Hakkar <51460023+Hakkarian@users.noreply.github.com>
2023-05-17 12:40:14 +0100

    fix

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -19,10 +19,8 @@ import { NewsList } from 'components/News/NewsList/NewsList';
 
 const NewsPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
-//   const [page, setPage] = useState(1); // setPage is defined but not used
+  const [page] = useState(1); 
   
-  const page = 1;
-
   const dispatch = useDispatch();
   const newsItems = useSelector(selectNews);
   const isLoading = useSelector(selectIsLoading);
