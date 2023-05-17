3163e0190af185613ca910a28ee2caf14129af74
S-Mariia <savchyn.mar@gmail.com>
2023-05-18 00:25:59 +0300

    add addaptive for add pet form

--- src/pages/AddPetPage/AddPetForm/Stepper/Stepper.jsx
+++ src/pages/AddPetPage/AddPetForm/Stepper/Stepper.jsx
@@ -1,9 +1,9 @@
 import { List, Item } from './Stepper.styled';
 const fields = ['Choose  option', 'Personal details', 'More info'];
 
-const Stepper = ({ currentStep }) => {
+const Stepper = ({ currentStep, category }) => {
   return (
-    <List currentStep={currentStep}>
+    <List currentStep={currentStep} category={category}>
       {fields.map((field, idx) => (
         <Item key={idx} idx={idx} currentStep={currentStep}>
           {field}
