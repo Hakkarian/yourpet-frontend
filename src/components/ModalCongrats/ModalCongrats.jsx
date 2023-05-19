371c269f0b056813748356f3b6ad3ba4e0c1c6b1
IrynaKniazieva <gleb0812@gmail.com>
2023-05-19 11:58:40 +0300

    fixed клік при натисканні логаут в модалці Логаут

--- src/components/ModalCongrats/ModalCongrats.jsx
+++ src/components/ModalCongrats/ModalCongrats.jsx
@@ -9,7 +9,7 @@ const ModalCongrats = ({ onClose }) => {
     <Container>
       <Title>Congrats!</Title>
       <Text>Youre registration is success</Text>
-      <Button className="btn" type="button" color="blue" width="248px">
+      <Button onClick={onClose} className="btn" type="button" color="blue" width="248px">
         Go to profile <Paw stroke="#FFFFFF" fill="none" width="24px" height="24px" />
       </Button>
     </Container>
@@ -22,61 +22,5 @@ ModalCongrats.propTypes = {
 
 export default ModalCongrats;
 
-
-// import React from 'react';
-// import { useEffect } from 'react';
-// import { createPortal } from 'react-dom';
-
-// // import CrossButton from 'shared/components/CrossButton/CrossButton';
-// import Button from 'shared/components/Button/Button';
-// import {
-//   Backdrop,
-//   Wrapper,
-//   Title,
-//   Text,
-//   CloseModalBtn,
-// } from './ModalCongrats.styled';
-// import { ReactComponent as Paw } from '../../icons/paw.svg';
-// import { ReactComponent as CrossSmall } from '../../icons/cross-small.svg';
-// // import plus from '../../icons/plus.svg';
-
-// const modalRoot = document.querySelector('#modal-root');
-
-// const ModalCongrats = ({ onClose, children, shouModal }) => {
-//   useEffect(() => {
-//     const handleKeyDowm = e => {
-//       if (e.code === 'Escape') onClose();
-//     };
-//     window.addEventListener('keydown', handleKeyDowm);
-//     return () => {
-//       window.removeEventListener('keydown', handleKeyDowm);
-//     };
-//   }, [onClose]);
-
-//   const handleBackdropClick = e => {
-//     if (e.currentTarget === e.target) {
-//       onClose();
-//     }
-//   };
-
-//   return createPortal(
-//     <Backdrop onClick={handleBackdropClick}>
-//       <Wrapper>
-//         <CloseModalBtn type="button" onClick={onClose}>
-//           <CrossSmall />
-//         </CloseModalBtn>
-//         {/* <CrossButton onClick={onClose}/> */}
-//         <Title>Congrats!</Title>
-//         <Text>Youre registration is success</Text>
-//         <Button type="button" color="white" width="248px" onClick={onClose}>
-//           Go to profile <Paw width="24px" height="24px" />
-//         </Button>
-//       </Wrapper>
-//     </Backdrop>,
-//     modalRoot
-//   );
-// };
-
-// export default ModalCongrats;
 
 
--- src/components/ModalLogOut/ModalLogOut.jsx
+++ src/components/ModalLogOut/ModalLogOut.jsx
@@ -4,7 +4,7 @@ import {ReactComponent as LogOutIcon} from '../../icons/logout.svg';
 import PropTypes from 'prop-types';
 
 
-const ModalLogOut = ({ onClose }) => {
+const ModalLogOut = ({ onClose, onClick }) => {
   return (
     <Container>
     <ModalTitle>Already leaving?</ModalTitle>
@@ -12,7 +12,7 @@ const ModalLogOut = ({ onClose }) => {
         <Button className="btn" type="button" color="white" width="256px" onClick={onClose}>
           Cancel
         </Button>
-        <Button className="btn" type="button" color="blue" width="256px">
+        <Button onClick={onClick} className="btn" type="button" color="blue" width="256px">
           Yes <LogOutIcon stroke="#FFFFFF" fill="none" className="svg" width="24px" height="24px" />
         </Button>
       </ContainerButton>
