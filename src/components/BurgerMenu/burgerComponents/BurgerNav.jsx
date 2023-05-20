3c755968703dfe814ac1a4ed24b67e89f4169dd6
Serhii Markov <goo.komalel@gmail.com>
2023-05-20 19:03:03 +0300

    burger done!

--- src/components/BurgerMenu/burgerComponents/BurgerNav.jsx
+++ src/components/BurgerMenu/burgerComponents/BurgerNav.jsx
@@ -1,19 +1,19 @@
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
-import ModalContext from "../../../shares/Modal/utils/modalContext";
+import ModalContext from "shared/components/Modalll/utils/modalContext";
 
-import sirojhkaData from "./sirojhka.json";
-// import navListData from "./headerNavList.json";
-
+import navListData from "./headerNavList.json";
 import { BurgerNavStyled } from "./burgerComp.styled";
 
 
 const BurgerNav = () => {
     const { close } = useContext(ModalContext);
 
-    const navList = sirojhkaData.map(({id, name}) => (
-        <NavLink onClick={close} key={id} to={name}>{name}</NavLink>
+    const navList = navListData.map(({id, name, path, className}) => (
+        <NavLink className={className}  onClick={close} key={id} to={path}>{name}</NavLink>
     ));
+
+
     return (
         <BurgerNavStyled>
             {navList}
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,17 +3,24 @@ import Header from 'components/Header/Header'
 import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 
+import ModalContext from 'shared/components/Modalll/utils/modalContext';
+import { useToggleModalContexst } from 'shared/components/Modalll/utils/useToggleModalContexst';
+
 
 const SharedLayout = () => {
+  const { isOpenContext, open, close, toggleContext } = useToggleModalContexst(false);
+
   return (
-    <div>
-      <Container>
-        <Header />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Outlet />
-        </Suspense>
-      </Container>
-    </div>
+    <ModalContext.Provider value={{ isOpenContext, open, close, toggleContext }}>
+      <div>
+        <Container>
+          <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </Container>
+      </div>
+    </ModalContext.Provider>
   );
 }
 
