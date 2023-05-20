29db7562300a7fdf347c36be8fcb1434e34af04c
Serhii Markov <goo.komalel@gmail.com>
2023-05-20 07:59:35 +0300

    button corection!

---
+++ src/shared/components/Button/buttonStyles/closeBtnStyles.js
@@ -0,0 +1,20 @@
+export const closeBtnStyles = ({ colors }) => `
+    width: 24px;
+    height: 24px;
+    border: 50%;
+    border: none;
+    background: ${colors.white};
+`;
+
+
+    // transition: transform 300ms cubic-bezier(0.4, 0, 0.2, 1);
+    // :hover, :focus {
+    //     transform: scale(1.3);
+    //     background: ${colors.yellow};
+    //     svg {
+    //         fill: ${colors.white};
+    //     }
+    // }
+    // svg {
+    //     fill: ${colors.yellow};
+    // }
