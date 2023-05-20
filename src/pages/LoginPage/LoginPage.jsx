dd5836bcbe5c879b5a2420fc3690da725350b51f
S-Mariia <savchyn.mar@gmail.com>
2023-05-20 21:16:41 +0300

    fixed login styles

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,7 +3,7 @@ import { CenrteredDiv } from './LoginPage.styled';
 
 import { useState } from 'react';
 
-import { ErrorMessage, Field, Formik } from 'formik';
+import { Field, Formik } from 'formik';
 import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
 
@@ -13,12 +13,20 @@ import { ReactComponent as EyeClosed } from '../../icons/eye-closed.svg';
 import { ReactComponent as Cross } from '../../icons/cross-small.svg';
 
 import { login } from 'redux/auth/auth-operations';
-import { Link, useNavigate } from 'react-router-dom';
-import { FormCss } from 'components/RegisterForm/RegisterForm.styled';
-import styled from '@emotion/styled';
-import ReusableButton from 'shared/components/ReusableButton';
-import ReusableTitle from 'shared/components/ReusableTitle';
-import { color, font } from 'shared/utils/mixin.styled';
+import { useNavigate } from 'react-router-dom';
+import {
+  FormCss,
+  AnchorCss,
+  RelativeDiv,
+  AbsDivCss,
+  ButtonEye,
+  FlexDivCss,
+  ReusableTitleCss,
+  TextWrapCss,
+  TextCss,
+  StyledLink,
+} from 'components/RegisterForm/RegisterForm.styled';
+import FormError from 'pages/AddPetPage/AddPetForm/FormFields/FormError/FormError';
 
 const validateShecma = Yup.object().shape({
   email: Yup.string().email('Invalid email address').required('Required'),
@@ -31,142 +39,6 @@ const validateShecma = Yup.object().shape({
     )
     .required('Required'),
 });
-
-export const FlexDivCss = styled.div`
-  margin-top: 40px;
-  margin-bottom: 40px;
-  padding: 40px 12px;
-
-  background-color: #fff;
-
-  border-radius: 20px;
-
-  @media screen and (min-width: 768px) {
-    padding: 60px 75px;
-  }
-`;
-
-export const InputEmail = styled(Field)`
-  width: 100%;
-  padding: 12px 16px;
-  border: 1px solid;
-
-  ::placeholder {
-  }
-  :focus {
-    outline: none;
-  }
-`;
-export const InputPassword = styled(Field)`
-  width: 100%;
-  padding: 12px 16px;
-  border: 1px solid;
-
-  ::placeholder {
-  }
-  :focus {
-    outline: none;
-  }
-`;
-export const InputContainer = styled.div`
-  display: flex;
-  margin-top: 40px;
-  margin-bottom: 60px;
-  flex-direction: column;
-  gap: 32px;
-  width: 100%;
-`;
-export const Text = styled.p`
-  line-height: 16px;
-
-  margin-top: 20px;
-`;
-export const StyledLink = styled(Link)``;
-export const Error = styled.p`
-  padding-left: 12px;
-  margin-top: 4px;
-`;
-export const Lable = styled.label`
-  position: relative;
-`;
-export const AbsoluteDiv = styled.div`
-  position: absolute;
-  right: 17px;
-  top: 17px;
-  transform: translate(35%, -35%);
-`;
-export const ToggleBtn = styled.button`
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  outline: none;
-`;
-export const ButtonEye = styled.button`
-  min-width: 0;
-  position: absolute;
-  top: 17%;
-  right: 0;
-  background-color: transparent;
-  border: none;
-`;
-
-export const AnchorCss = styled.div`
-  position: relative;
-`;
-
-export const LabelCss = styled.label`
-  position: absolute;
-  top: 50%;
-  left: 10%;
-  transform: translate(0%, -50%);
-`;
-
-export const ReusableButtonCss = styled(ReusableButton)`
-  width: 100%;
-
-  ${color('#fff', ({ theme }) => theme.colors.blue)}
-`;
-export const AbsDivCss = styled.span`
-  position: absolute;
-  top: 17%;
-  right: 10%;
-`;
-
-export const ReusableTitleCss = styled(ReusableTitle)`
-  margin-top: 0;
-  margin-bottom: 30px;
-
-  text-align: center;
-
-  font-weight: 500;
-  line-height: 1.375;
-
-  @media screen and (min-width: 768px) {
-    margin-top: 0;
-    line-height: 1.361;
-  }
-`;
-
-export const TextWrapCss = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  margin: 0;
-
-  font-family: 'Manrope';
-  font-size: 12px;
-  font-weight: 400;
-`;
-
-export const TextCss = styled.p`
-  margin: 0;
-`;
-
-export const ErrorText = styled.div`
-  position: absolute;
-  font-size: 12px;
-  ${font(null, 1.3, 500, '0.03em', 'red')}
-`;
 
 const LoginPage = () => {
   const [open, setOpen] = useState(false);
@@ -186,38 +58,63 @@ const LoginPage = () => {
       <FlexDivCss>
         <ReusableTitleCss>Login</ReusableTitleCss>
         <Formik
-          initialValues={{ email: '', password: '', confirmPassword: '' }}
+          initialValues={{ email: '', password: '' }}
+          validateOnBlur={false}
+          validateOnMount={false}
           validationSchema={validateShecma}
           onSubmit={handleSubmit}
         >
-          {({ errors, values, touched }) => {
+          {({ errors, values, touched, setFieldValue }) => {
             return (
               <FormCss>
                 <AnchorCss>
-                  <Field
-                    name="email"
-                    type="email"
-                    className={errors.email ? 'input-error' : 'input-valid'}
-                  />
-                  <ErrorText>
-                    <ErrorMessage name="email" />
-                  </ErrorText>
-                  {errors.email && (
-                    <AbsDivCss>
-                      <Cross width="24" height="24" stroke="#F43F5E" />
-                    </AbsDivCss>
-                  )}
+                  <RelativeDiv>
+                    <Field
+                      name="email"
+                      type="email"
+                      placeholder="Email"
+                      className={
+                        errors.email && touched.email
+                          ? 'input-error'
+                          : 'input-valid'
+                      }
+                    />
+                    {errors.email && values.email && (
+                      <AbsDivCss
+                        onClick={() => {
+                          setFieldValue('email', '');
+                        }}
+                      >
+                        <Cross width="24" height="24" stroke="#F43F5E" />
+                      </AbsDivCss>
+                    )}
+                  </RelativeDiv>
+                  <FormError name="email" />
                 </AnchorCss>
-                <AnchorCss>
-                  <Field
-                    className={errors.password ? 'input-error' : 'input-valid'}
-                    name="password"
-                    type={open ? 'text' : 'password'}
-                  />
-                  <ErrorText>
-                    <ErrorMessage name="password" />
-                  </ErrorText>
 
+                <AnchorCss>
+                  <RelativeDiv>
+                    <Field
+                      className={
+                        errors.password && touched.password
+                          ? 'input-error'
+                          : 'input-valid'
+                      }
+                      name="password"
+                      placeholder="Password"
+                      type={open ? 'text' : 'password'}
+                    />
+                    {errors.password && values.password && (
+                      <AbsDivCss
+                        onClick={() => {
+                          setFieldValue('password', '');
+                        }}
+                      >
+                        <Cross width="24" height="24" stroke="#F43F5E" />
+                      </AbsDivCss>
+                    )}
+                  </RelativeDiv>
+                  <FormError name="password" />
                   {open ? (
                     <ButtonEye
                       className="eye-button"
@@ -231,18 +128,14 @@ const LoginPage = () => {
                       <EyeClosed width="24" height="24" />
                     </ButtonEye>
                   )}
-                  {errors.password && (
-                    <AbsDivCss>
-                      <Cross width="24" height="24" stroke="#F43F5E" />
-                    </AbsDivCss>
-                  )}
                 </AnchorCss>
+
                 <Button className="form-button" type="submit">
                   Submit
                 </Button>
                 <TextWrapCss>
                   <TextCss>Don't have an account?</TextCss>
-                  <Link to="/register">Register</Link>
+                  <StyledLink to="/register">Register</StyledLink>
                 </TextWrapCss>
               </FormCss>
             );
