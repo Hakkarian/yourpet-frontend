18ac2b6b2dfff0e6076f7cd1ccc4c19bd806c9ed
Hakkar <zugrutus@gmail.com>
2023-05-21 19:07:32 +0100

    fix

--- src/components/Notices/AddPetButton/AddPetBtnMobile.jsx
+++ src/components/Notices/AddPetButton/AddPetBtnMobile.jsx
@@ -4,14 +4,14 @@ import {
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/auth/auth-selector';
 import { useState } from 'react';
-import { useToggle } from 'shared/hooks/useToggle';
+// import { useToggle } from 'shared/hooks/useToggle';
 import ModalAddPet from 'components/Modals/ModalAddPet';
 import { AddCss } from './AddPetBtn.styled';
 import { useNavigate } from 'react-router-dom';
 
 const AddPetButtonMobile = () => {
   const isLogined = useSelector(selectIsLoggedIn);
-  const { open } = useToggle();
+  // const { open } = useToggle();
   const [open1, setOpen1] = useState(false);
   const navigate = useNavigate()
 
