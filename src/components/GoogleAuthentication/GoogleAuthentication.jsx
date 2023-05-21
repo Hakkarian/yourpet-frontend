c08a32abdd109bb7794cdb51ac53e4bf6bcaa6cc
Hakkar <zugrutus@gmail.com>
2023-05-21 22:52:30 +0100

    fix

--- src/components/GoogleAuthentication/GoogleAuthentication.jsx
+++ src/components/GoogleAuthentication/GoogleAuthentication.jsx
@@ -1,43 +1,43 @@
-import React, { useEffect } from 'react'
-import jwt_decode from 'jwt-decode';
-import { useDispatch } from 'react-redux';
-import { googleAuth } from 'redux/auth/auth-slice';
+// import React, { useEffect } from 'react'
+// import jwt_decode from 'jwt-decode';
+// import { useDispatch } from 'react-redux';
+// import { googleAuth } from 'redux/auth/auth-slice';
 
 const GoogleAuthentication = () => {
-   const dispatch = useDispatch();
+  //  const dispatch = useDispatch();
 
-     useEffect(() => {
-         /* global google */
-         google.accounts.id.initialize({
-           client_id:
-                 '636271452984-is9qjcnf72o5964ambhhdp1q2cbtlfcu.apps.googleusercontent.com',
-             callback: handleCallbackResponse
-         });
-         google.accounts.id.renderButton(
-             document.getElementById('signInDiv'),
-             {theme: 'outline', size: 'large'}
-       )
+  //    useEffect(() => {
+  //        /* global google */
+  //        google.accounts.id.initialize({
+  //          client_id:
+  //                '636271452984-is9qjcnf72o5964ambhhdp1q2cbtlfcu.apps.googleusercontent.com',
+  //            callback: handleCallbackResponse
+  //        });
+  //        google.accounts.id.renderButton(
+  //            document.getElementById('signInDiv'),
+  //            {theme: 'outline', size: 'large'}
+  //      )
       
-       google.accounts.id.prompt()
-     }, [])
+  //      google.accounts.id.prompt()
+  //    }, [])
 
-  const handleCallbackResponse = (response) => {
-     const info = jwt_decode(response.credential);
-     const payload = {
-       token: response.credential,
-       user: {
-         email: info.email,
-         sub: info.sub
-       }
-    }
-    // const password = `HASH${Math.floor(Math.random(100))}hm`
-    console.log(payload)
-    dispatch(googleAuth(payload))
+  // const handleCallbackResponse = (response) => {
+  //    const info = jwt_decode(response.credential);
+  //    const payload = {
+  //      token: response.credential,
+  //      user: {
+  //        email: info.email,
+  //        sub: info.sub
+  //      }
+  //   }
+  //   // const password = `HASH${Math.floor(Math.random(100))}hm`
+  //   console.log(payload)
+  //   dispatch(googleAuth(payload))
     
-     }
-   return (
-     <div onClick={handleCallbackResponse} id='signInDiv'></div>
-   )
+  //    }
+  //  return (
+  //    <div onClick={handleCallbackResponse} id='signInDiv'></div>
+  //  )
 }
 
 export default GoogleAuthentication
