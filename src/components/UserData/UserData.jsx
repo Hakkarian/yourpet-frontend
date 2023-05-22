2bca1aa7c89255858e591ea97747b16c8a758222
Hakkar <zugrutus@gmail.com>
2023-05-22 22:42:49 +0100

    fix

--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { selectUser } from 'redux/auth/auth-selector';
