13c6e6c07ebe176a2878fea8638746f205d3b037
Hakkar <zugrutus@gmail.com>
2023-05-31 19:04:40 +0100

    avatar-tablet-fix

--- src/components/BurgerMenu/burgerComponents/BurgerUser.jsx
+++ src/components/BurgerMenu/burgerComponents/BurgerUser.jsx
@@ -4,18 +4,25 @@ import { ModalContext } from 'shared/components/Modalll/utils/ModalProvider';
 
 import { ReactComponent as User } from '../img/user.svg';
 import { BurgerUserStyled } from './burgerComp.styled';
+import { useSelector } from 'react-redux';
+import { selectUser } from 'redux/auth/auth-selector';
+import { ImgCss } from 'components/Header/Header.styled';
 
 const BurgerUser = ({ userName }) => {
     const { close } = useContext(ModalContext);
+    const avatar = useSelector(selectUser);
     
     return (
-        <BurgerUserStyled>
-            <Link onClick={close} to="/user">
-                <User/>
-                <p>{userName}</p>
-            </Link>
-        </BurgerUserStyled>
-    )
+      <BurgerUserStyled>
+          <Link onClick={close} to="/user">
+                {!avatar && <User />}
+            {avatar && (
+              <ImgCss src={avatar} alt="user" width="30" height="30" />
+            )}
+            <p>{userName}</p>
+          </Link>
+      </BurgerUserStyled>
+    );
 };
 BurgerUser.defaultProps = {
     userName: 'no name',
