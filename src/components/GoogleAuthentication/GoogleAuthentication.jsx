ae5741158759d21aa874060566158887ea2941ac
Hakkar <zugrutus@gmail.com>
2024-04-15 11:32:47 +0100

    fixed frontend google oauth

--- src/components/GoogleAuthentication/GoogleAuthentication.jsx
+++ src/components/GoogleAuthentication/GoogleAuthentication.jsx
@@ -5,10 +5,9 @@ import google from '../../images/logo/google-logo.png';
 const GoogleAuthentication = () => {
 
   const frontendUrl = process.env.REACT_APP_API_URL;
-  console.log(`${frontendUrl}/auth/google`);
   
   return (
-    <GoogleCss href={`${frontendUrl}/auth/google`}>
+    <GoogleCss href={`${frontendUrl}/api/auth/google`}>
       <img src={google} alt="Google logo" width="30" height="30" /> Authenticate
       with Google
     </GoogleCss>
